f094dfba1500da38cec104518337851a63c1f5fd
Gururaj8756 <107394510+Gururaj8756@users.noreply.github.com>
2024-03-26 00:41:35 +0530

    Login API

--- auth_module/views.py
+++ auth_module/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from auth_module.models import User123
 from django.contrib.auth.hashers import make_password
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
 class CreateUserView(APIView):
     def post(self,request,*args,**kwargs):
         data = request.data        
         student_name = data['student_name']
-        password=data['pass']
+        password=data['password']
         email = data['email']
         dob=data['dob']
         new_user = User123.objects.create(
@@ -40,3 +42,20 @@ def create_user(request):
         new_user.save()
         return render(request, 'success.html')
     return render(request, 'user_form.html')
+
+class LoginUserView(APIView):
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Authenticate user
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            # User authenticated successfully
+            # You can perform additional tasks here, such as generating tokens
+            # For now, let's just return a success message
+            return Response({'message': 'Login successful'})
+        else:
+            # Authentication failed
+            return Response({'message': 'Invalid credentials'}, status=401)
