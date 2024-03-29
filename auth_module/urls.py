78fabd81bac700d6b06efff3b4cc8e1b3c408f25
Gururaj8756 <107394510+Gururaj8756@users.noreply.github.com>
2024-03-29 23:36:21 +0530

    url

--- auth_module/urls.py
+++ auth_module/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('login/', UserLoginAPIView.as_view(), name='login'),
     path('question/', my_question, name='question'),
     path('tcs/', tcs_details, name='tcs'),
-    path('accenture/', accenture_details, name='hsbc'),
-    path('hsbc/', accenture_details, name='tcs'),
+    path('accenture/', accenture_details, name='accenture'),
+    path('hsbc/', HSBC_details, name='hsbc'),
  ]
