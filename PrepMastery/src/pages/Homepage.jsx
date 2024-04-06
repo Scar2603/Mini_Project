50b2f3aad901d02eb129e1d7e57486b35af6015a
yash@8123 <116726926+yash08123@users.noreply.github.com>
2024-04-06 10:46:44 +0530

    Fix formatting issues and remove unused code

--- PrepMastery/src/pages/Homepage.jsx
+++ PrepMastery/src/pages/Homepage.jsx
@@ -1,29 +1,18 @@
-import React ,{useEffect} from 'react'
+import React from 'react'
 import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import About from '../components/About'
 import Team from '../components/Team'
 import Tracks from '../components/Tracks'
-import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { useSelector } from 'react-redux';
 import Contact from '../components/Contact'
 
 function Homepage() {
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      toast.success("Welcome back! You have logged in successfully.");
-    }
-  }, [isAuthenticated]); // Depend on isAuthenticated
-
   
   return (
     <div>
         <Nav/>
-        <ToastContainer autoClose={1000}/>
         <Hero/>
         <Tracks/>
         <About/>
